1050e10c9213826fd8169e951dbfb5cff039c695
John McKim <johncmckim@gmail.com>
2015-09-16 14:14:29 +1000

    Add example

---
+++ example/HalcyonExample/App_Start/WebApiConfig.cs
@@ -0,0 +1,29 @@
+﻿using Halcyon.HAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+
+namespace HalcyonExample {
+    public static class WebApiConfig {
+        public static void Register(HttpConfiguration config) {
+            // Web API configuration and services
+
+            // Web API routes
+            config.MapHttpAttributeRoutes();
+
+
+            config.Formatters.RemoveAt(0);
+            config.Formatters.Insert(0, new JsonHALMediaTypeFormatter(
+                halJsonMedaiTypes: new string[] { "application/hal+json", "application/vnd.example.hal+json", "application/vnd.example.hal.v1+json" },
+                jsonMedaiTypes: new string[] { "application/vnd.example+json", "application/vnd.example.v1+json" }
+            ));
+
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+        }
+    }
+}
---
+++ example/HalcyonExample/Controllers/FooController.cs
@@ -0,0 +1,57 @@
+﻿using Halcyon.HAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace HalcyonExample.Controllers {
+    [RoutePrefix("api/foo")]
+    public class FooController : ApiController {
+
+        [HttpGet, Route("{id:int}")]
+        public IHttpActionResult Get(int id) {
+            // Any plain old object will do
+            var fooModel = new {
+                id = id,
+                type = "foo"
+            };
+            
+            // Return a simple resource with links to related resources
+            return this.HAL(fooModel, new Link[] {
+                new Link("self", "/api/foo/{id}"),
+                new Link("foo:bar", "/api/foo/{id}/bar")
+            });
+        }
+
+        [HttpGet, Route("{fooId:int}/bars")]
+        public IHttpActionResult GetBar(int fooId) {
+            // A collection of bars related to foo
+            var bars = new List<object> {
+                new { id = 1, fooId = fooId, type = "bar" },
+                new { id = 2, fooId = fooId, type = "bar" }
+            };
+
+            // data about the bars related to foo
+            var fooBarModel = new {
+                fooId = fooId,
+                count = bars.Count
+            };
+
+            // Return a fooBar resource with embedded bars
+            return this.HAL(
+                fooBarModel,
+                new Link[] {
+                    new Link("self", "/api/foo/{fooId}/bar")
+                },
+                "bars",
+                bars,
+                new Link[] {
+                    new Link("self", "/api/bar/{id}")
+                }
+            );
+        }
+    }
+}
+
