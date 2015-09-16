264d1c9e5056cf15ef3bc68876c17f9b98f7a918
John McKim <johncmckim@gmail.com>
2015-09-16 11:33:32 +1000

    Use URI Template Nuget package and add nuspec

--- src/Halcyon/Templates/TemplateExtensions.cs
+++ src/Halcyon/Templates/TemplateExtensions.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Tavis.UriTemplates;
 
 namespace Halcyon.Templates {
     public static class TemplateExtensions {
