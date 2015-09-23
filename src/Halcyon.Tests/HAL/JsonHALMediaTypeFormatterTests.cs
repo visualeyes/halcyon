1dbbd9982473b1cbc1c56f0fc97ef7e08cbf369f
John McKim <johncmckim@gmail.com>
2015-09-23 14:30:38 +1000

    Add unit test for writing to stream

--- src/Halcyon.Tests/HAL/JsonHALMediaTypeFormatterTests.cs
+++ src/Halcyon.Tests/HAL/JsonHALMediaTypeFormatterTests.cs
@@ -1,7 +1,10 @@
 ﻿using Halcyon.HAL;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net;
+using System.Net.Http;
 using System.Net.Http.Formatting;
 using System.Text;
 using System.Threading.Tasks;
@@ -48,6 +51,32 @@ namespace Halcyon.Tests.HAL {
 
             Assert.NotNull(formatter);
             Assert.Contains("application/test", formatter.SupportedMediaTypes.Select(m => m.MediaType));
+        }
+
+
+        [Fact]
+        public async Task Write_To_Stream_test() {
+            using (var stream = new MemoryStream()) {
+                var content = new StringContent("");
+
+                var formatter = new JsonHALMediaTypeFormatter();
+                
+                var model = new HALModel(new {
+                    test = 1
+                });
+
+                Assert.NotNull(formatter);
+
+                await formatter.WriteToStreamAsync(typeof(HALModel), model, stream, content, null);
+
+                // Reset the position to ensure it can read
+                stream.Position = 0;
+
+                var reader = new StreamReader(stream);
+                string result = await reader.ReadToEndAsync();
+
+                Assert.Equal("{\"test\":1}", result);
+            }
         }
     }
 }
