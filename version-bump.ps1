bba1ac454ce0d5fff4017e0a587cbb705f2a8d5e
John McKim <johncmckim@gmail.com>
2016-03-24 13:57:10 +1000

    Add logging to verion bump script

--- version-bump.ps1
+++ version-bump.ps1
@@ -10,10 +10,13 @@ $projectFilePath = Join-Path $buildFolder "src\Halcyon.Mvc\project.json"
 
 Write-Host "Bumping version for project file: $projectFilePath to $buildNumber"
 
-$projectConfig = Get-Content $projectFilePath | 
-					ConvertFrom-Json
+$projectFileContent = Get-Content $projectFilePath
 
-$projectConfig.version = $buildNumber
+Write-Host $projectFileContent
+
+$projectConfig = $projectFileContent | ConvertFrom-Json
+
+$projectConfig.version = "$buildNumber-alpha"
 
 $projectConfig | 
 	ConvertTo-Json |
