adb6faec2ef6a92b102afa35042ae7556de68218
Niklas Arbin <niklas.arbin@bonnierbooks.se>
2016-04-11 12:15:27 +0200

    HalModelAttribute should only be valid on class

--- src/Halcyon.WebApi/HAL/Filters/HalModelAttribute.cs
+++ src/Halcyon.WebApi/HAL/Filters/HalModelAttribute.cs
@@ -4,7 +4,7 @@ using Halcyon.HAL.Filters;
 
 namespace Halcyon.WebApi.HAL.Filters
 {
-    [AttributeUsage(AttributeTargets.Method | AttributeTargets.Class, AllowMultiple = false, Inherited = true)]
+    [AttributeUsage(AttributeTargets.Class, AllowMultiple = false, Inherited = true)]
     public class HalModelAttribute : ActionFilterAttribute, IHalModelAttribute
     {
         public string LinkBase { get; }
@@ -26,6 +26,23 @@ namespace Halcyon.WebApi.HAL.Filters
         public string Method { get; }
 
         public HalLinkAttribute(string rel, string href, string title = null, string method = null)
+        {
+            Rel = rel;
+            Href = href;
+            Title = title;
+            Method = method;
+        }
+    }
+
+    [AttributeUsage(AttributeTargets.Method | AttributeTargets.Class, AllowMultiple = true, Inherited = true)]
+    public class HalEmbeddedAttribute : ActionFilterAttribute
+    {
+        public string Rel { get; }
+        public string Href { get; }
+        public string Title { get; }
+        public string Method { get; }
+
+        public HalEmbeddedAttribute(string rel, string href, string title = null, string method = null)
         {
             Rel = rel;
             Href = href;
