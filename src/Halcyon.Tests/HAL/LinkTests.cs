ae86bc729538b097abc796e55caaafd671601efa
John McKim <johncmckim@gmail.com>
2016-05-10 10:43:35 +1000

    Add support for templates in Link titles #18

--- src/Halcyon.Tests/HAL/LinkTests.cs
+++ src/Halcyon.Tests/HAL/LinkTests.cs
@@ -10,21 +10,31 @@ namespace Halcyon.Tests.HAL {
     public class LinkTests {
 
         [Theory]
-        [InlineData(null, null, null, null)]
-        [InlineData("", "", "", "")]
-        [InlineData("rel", "href", "title", "method")]
-        public void Link_Constructed(string rel, string href, string title, string method) {
+        [InlineData(null, null, null, null, null, null, null, null, null)]
+        [InlineData("", "", "", "", "", "", "", "", "")]
+        [InlineData("rel", "href", "title", "method", "type", "deprecation", "name", "profile", "hrefLang")]
+        public void Link_Created(string rel, string href, string title, string method, string type, string deprecation, string name, string profile, string hrefLang) {
             var link = new Link(
                 rel: rel,
                 href: href,
                 title: title,
                 method: method
-            );
+            ) {
+                Type = type,
+                Deprecation = deprecation,
+                Name = name,
+                Profile = profile,
+                HrefLang = hrefLang
+            };
 
             Assert.Equal(rel, link.Rel);
             Assert.Equal(href, link.Href);
             Assert.Equal(method, link.Method);
             Assert.Equal(title, link.Title);
+            Assert.Equal(type, link.Type);
+            Assert.Equal(deprecation, link.Deprecation);
+            Assert.Equal(name, link.Name);
+            Assert.Equal(hrefLang, link.HrefLang);
         }
 
         [Theory]
@@ -41,20 +51,21 @@ namespace Halcyon.Tests.HAL {
             Assert.Equal(href, link.Href);
             Assert.Equal(isTemplated, link.Templated);
         }
-
+        
         [Theory]
-        [InlineData(null, null, null, true)]
-        [InlineData("", "", null, true)]
-        [InlineData("href/{one}", "href/1", null, true)]
-        [InlineData("href/{one}", "href/{one}", true, false)]
-        [InlineData("href/{two}", "href/", null, true)]
-        public void Create_Link(string href, string expectedHref, bool? isTemplated, bool replaceParameters) {
+        [InlineData(null, null, null, null, null, true)]
+        [InlineData("", "", "", "", null, true)]
+        [InlineData("href/{one}", "href/1", "book-{one}", "book-1", null, true)]
+        [InlineData("href/{one}", "href/{one}", "book-{one}", "book-{one}", true, false)]
+        [InlineData("href/{two}", "href/", "book-{two}", "book-", null, true)]
+        public void Create_Link(string href, string expectedHref, string title, string expectedTitle, bool? isTemplated, bool replaceParameters) {
             var parameters = new Dictionary<string, object> {
                 { "one", 1 }
             };
 
             var link = new Link(
                 rel: "self",
+                title: title,
                 href: href,
                 replaceParameters: replaceParameters
             );
@@ -62,6 +73,7 @@ namespace Halcyon.Tests.HAL {
             var templatedLink = link.CreateLink(parameters);
 
             Assert.Equal(expectedHref, templatedLink.Href);
+            Assert.Equal(expectedTitle, templatedLink.Title);
             Assert.Equal(isTemplated, templatedLink.Templated);
         }
 
