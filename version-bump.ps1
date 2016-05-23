15dd4e247825fb7a22cbe9df175f392039fa36bb
John McKim <johncmckim@gmail.com>
2016-05-23 16:45:18 +1000

    Get version-bump to check branch and tags

--- version-bump.ps1
+++ version-bump.ps1
@@ -1,6 +1,15 @@
 $ErrorActionPreference = "Stop"
 
 $buildNumber = $env:APPVEYOR_BUILD_VERSION
+
+if ($env:APPVEYOR_REPO_BRANCH -eq 'develop') {
+	$buildNumber = "$buildNumber-beta"
+}
+
+if ($env:APPVEYOR_REPO_TAG_NAME) {
+	$buildNumber = $env:APPVEYOR_REPO_TAG_NAME
+}
+
 $buildFolder = '.'
 
 if($env:APPVEYOR_BUILD_FOLDER) {
@@ -14,6 +23,6 @@ Write-Host "Bumping version to $buildNumber"
 $projectFilePaths | 
   Foreach {
 	(gc -Path $_) `
-	  -replace "(?<=`"version`":\s`")[.\w-]*(?=`",)", "$buildNumber-alpha" |
+	  -replace "(?<=`"version`":\s`")[.\w-]*(?=`",)", "$buildNumber" |
 	  sc -Path $_ -Encoding UTF8
   }
