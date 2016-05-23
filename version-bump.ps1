34730970449f4e8d90878f1cb46e22aaae4c55d0
John McKim <johncmckim@gmail.com>
2016-05-24 08:57:04 +1000

    Get version-bump to check branch and tags (#29)
    * Get version-bump to check branch and tags
    
    * Add branch to version if not master
    
    * Fix version bump

--- version-bump.ps1
+++ version-bump.ps1
@@ -1,6 +1,17 @@
 $ErrorActionPreference = "Stop"
 
 $buildNumber = $env:APPVEYOR_BUILD_VERSION
+
+if ($env:APPVEYOR_REPO_BRANCH -eq 'develop') {
+	$buildNumber = "$buildNumber-beta"
+} elseif ($env:APPVEYOR_REPO_BRANCH -ne 'master') {
+	$buildNumber = "$buildNumber-$env:APPVEYOR_REPO_BRANCH"
+}
+
+if ($env:APPVEYOR_REPO_TAG_NAME) {
+	$buildNumber = $env:APPVEYOR_REPO_TAG_NAME
+}
+
 $buildFolder = '.'
 
 if($env:APPVEYOR_BUILD_FOLDER) {
@@ -14,6 +25,6 @@ Write-Host "Bumping version to $buildNumber"
 $projectFilePaths | 
   Foreach {
 	(gc -Path $_) `
-	  -replace "(?<=`"version`":\s`")[.\w-]*(?=`",)", "$buildNumber-alpha" |
+	  -replace "(?<=`"version`":\s`")[.\w-]*(?=`",)", "$buildNumber" |
 	  sc -Path $_ -Encoding UTF8
   }
