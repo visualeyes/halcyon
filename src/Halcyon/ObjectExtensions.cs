524fd89284e3f8da5c77a68f6188272ca3c89a74
John McKim <johncmckim@gmail.com>
2016-08-08 16:42:30 +1000

    Add tests

--- src/Halcyon/ObjectExtensions.cs
+++ src/Halcyon/ObjectExtensions.cs
@@ -9,29 +9,33 @@ namespace Halcyon {
         internal static IDictionary<string, object> ToDictionary(this object obj) {
             IDictionary<string, object> vardic;
 
-            if (obj is IDictionary<string, object>) {
+            if(obj is IDictionary<string, object>) {
                 vardic = (IDictionary<string, object>)obj;
-            } else if (obj is JObject) {
-                var jObj = ((JObject)obj);
-                vardic = new Dictionary<string, object>(StringComparer.OrdinalIgnoreCase);
-
-                var properties = jObj.Properties();
-
-                foreach (var prop in properties) {
-                    var objValue = prop.Value;
-
-                    vardic.Add(prop.Name, objValue);
-                }
+            } else if(obj is JObject) {
+                vardic = ToDictionary((JObject)obj);
             } else {
                 vardic = new Dictionary<string, object>(StringComparer.OrdinalIgnoreCase);
 
                 var properties = obj.GetType().GetProperties();
 
-                foreach (var prop in properties) {
+                foreach(var prop in properties) {
                     var objValue = prop.GetValue(obj, null);
 
                     vardic.Add(prop.Name, objValue);
                 }
+            }
+
+            return vardic;
+        }
+
+        internal static IDictionary<string, object> ToDictionary(this JObject obj) {
+            var vardic = new Dictionary<string, object>(StringComparer.OrdinalIgnoreCase);
+
+            var properties = obj.Properties();
+
+            foreach(var prop in properties) {
+                var objValue = prop.Value;
+                vardic.Add(prop.Name, objValue);
             }
 
             return vardic;
